d8772552813173e0ad52e353771a54f069e8faed
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-03-09 18:31:59 +0300

    Add encrypt function

--- LW2 (Mikozi)/main.cpp
+++ LW2 (Mikozi)/main.cpp
@@ -7,26 +7,85 @@
 
 #include <iostream>
 #include <vector>
+#include <bitset>
 
-std::vector <int> convertToBinary(int &num) {
-    std::vector <int> numBinary;
-    for (int i = 0; num > 0; ++i) {
-        numBinary.push_back(num % 2);
-        num /= 2;
+bool findBit(int num, int bit) {
+    return (num & (1 << (bit - 1))) != 0;
+}
+
+void setBit(int &num, int bit) {
+    num |= 1 << (bit - 1);
+}
+
+std::vector <int> convertToBinary2(int num) {
+    std::vector <int> keyNumBinary1 = {1, 2, 3, 4, 5, 6, 7, 8};
+    std::vector <int> keyNumBinary2 = {1, 2, 3, 4, 9, 10, 11, 12};
+    std::vector <int> keyNumBinary3 = {5, 6, 7, 8, 12, 11, 10, 9};
+    std::vector <std::vector <int>> keyNumBinary = {keyNumBinary1, keyNumBinary2, keyNumBinary3};
+    
+    std::vector <int> keyBinary (3);
+    
+    for (int k = 0; k < 3; ++k) {
+        for (int i = 0; i < 8; ++i) {
+            if (findBit(num, 13 - keyNumBinary[k][i])) {
+                setBit(keyBinary[k], 8 - i);
+            }
+        }
     }
-    reverse(numBinary.begin(), numBinary.end());
-    return numBinary;
+    return keyBinary;
+}
+
+int shuffleNum(int num) {
+    std::vector <int> keyNumBinary = {1, 4, 7, 2, 5, 8, 3, 6};
+    
+    int resNum = 0;
+    
+    for (int i = 0; i < 8; ++i) {
+        if (findBit(num, 9 - keyNumBinary[i])) {
+            setBit(resNum, 8 - i);
+        }
+    }
+
+    return resNum;
+}
+
+
+void splitNum(int num, int &part1, int &part2) {
+    part1 = 0b11110000;
+    part2 = 0b00001111;
+    
+    part1 &= num;
+    part2 &= num;
+    
+    part1 >>= 4;
 }
 
 void printBinaryNum(std::vector <int> &numBinary) {
     for (int i = 0; i < numBinary.size(); ++i) {
-        std::cout << numBinary[i] << ' ';
+        std::cout << std::bitset<8>(numBinary[i]) << ' ';
     }
     std::cout << '\n';
 }
 
+int encryptMessage(std::vector <int> keyBinary, int num) {
+    std::vector <int> s1 = {14, 4, 6, 2, 11, 3, 13, 8, 12, 15, 5, 10, 0, 7, 1, 9};
+    std::vector <int> s2 = {14, 7, 10, 12, 13, 1, 3, 9, 0, 2, 11, 4, 15, 8, 5, 6};
+    
+    int part1 {0};
+    int part2 {0};
+    
+    for (int i = 0; i < 3; ++i) {
+        num += keyBinary[i];
+        splitNum(num, part1, part2);
+        num = s1[part1] << 4 ^ s2[part2];
+        num = shuffleNum(num);
+    }
+    return num;
+}
+
 int main(int argc, const char * argv[]) {
 
+    /*
     std::cout << "Programm start!\n";
     std::cout << "N = 16, 7 * 16 = 102\n";
     int num = 102;
@@ -38,5 +97,25 @@ int main(int argc, const char * argv[]) {
     int keyNum = 3634;
     std::vector <int> keyNumBinary = convertToBinary(keyNum);
     printBinaryNum(keyNumBinary);
+    return 0;
+     
+    */
+    int num = 102;
+    int keyNum = 3634;
+    
+    std::vector <int> keyBinary = convertToBinary2(keyNum);
+    
+    printBinaryNum(keyBinary);
+    
+    int numChangeBit = num ^ 1;
+    
+    num = encryptMessage(keyBinary, num);
+    
+    numChangeBit = encryptMessage(keyBinary, numChangeBit);
+    
+    std::cout << std::bitset<8>(num) << "\n";
+    
+    std::cout << std::bitset<8>(numChangeBit) << "\n";
+    
     return 0;
 }
